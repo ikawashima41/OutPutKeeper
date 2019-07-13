9a4a1c6277c2413c265c408f6dd655b4b360037f
kawashima41 <i.kawashima41@gmail.com>
2019-07-13 13:46:53 +0900

    Initial Commit

---
+++ OutPutKeeper/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  OutPutKeeper
+//
+//  Created by Iichiro Kawashima on 2019/07/13.
+//  Copyright © 2019 Iichiro Kawashima. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
